f636be18b352b4a5761f99af0c16fbeefa584268
juangpartida <juangpartida@protonmail.com>
2022-04-10 19:43:45 -0700

    Initial Commit

---
+++ Flashcard App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcard App
+//
+//  Created by Juan G. Partida on 4/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Flashcard AppUITests/Flashcard_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flashcard_AppUITestsLaunchTests.swift
+//  Flashcard AppUITests
+//
+//  Created by Juan G. Partida on 4/10/22.
+//
+
+import XCTest
+
+class Flashcard_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
